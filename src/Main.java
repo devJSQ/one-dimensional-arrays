b5fb0cf0f56c5ae00e44ad47bfa75b8e652b0890
JSQdev <148073798+devJSQ@users.noreply.github.com>
2024-03-16 01:19:48 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
       String [] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
       System.out.println(weekdays.length);
       System.out.println(weekdays[weekdays.length - 1]);
-    
-    }
+      for (int x = 0; x < weekdays.length; x ++){
+          System.out.println(weekdays[x]);
+   }
 
 }
